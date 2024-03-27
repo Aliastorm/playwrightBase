05547b88ffd1857a6f50f39754857424012bd467
jfpons <jf.pons@gmail.com>
2024-03-27 18:59:45 +0100

    lint

--- src/pom/SamplePage.ts
+++ src/pom/SamplePage.ts
@@ -1,18 +1,18 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page } from '@playwright/test';
 
 export class SamplePage {
-  private loginField: Locator;
+    private loginField: Locator;
 
-  constructor(protected page: Page) {
-    this.page = page;
-    this.loginField = this.page.locator('input[name="login"]');
-  }
+    constructor(protected page: Page) {
+        this.page = page;
+        this.loginField = this.page.locator('input[name="login"]');
+    }
 
-  fillLoginField = async (login: string) => {
-    await this.loginField.fill(login);
-  };
+    fillLoginField = async (login: string) => {
+        await this.loginField.fill(login);
+    };
 
-  myMethod = async () => {
-    await this.fillLoginField("test");
-  };
+    myMethod = async () => {
+        await this.fillLoginField('test');
+    };
 }
